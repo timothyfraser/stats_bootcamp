c8cdbe2c70a2570a7237cb146aa03c401cabc7c0
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2024-12-26 22:35:39 +0000

    .

---
+++ D/D1_code.py
@@ -0,0 +1,10 @@
+#' @name D1_code.py
+#' @author Prof. Tim Fraser
+#' @description
+#' Topic: Data Wrangling
+#' 
+#' Sometimes we need to edit and arrange our data better.
+#' We call this 'data wrangling,' and it's a super useful skill.
+#' Small functions can save you dozens of hours of work,
+#' and it makes you really employable. 
+
