eca2e0e0ba520866140d117eeedf0ea5b9662fb0
Timothy Fraser <timothy.fraser.1@gmail.com>
2025-01-14 17:24:52 +0000

    .

--- K/K2_code.R
+++ K/K2_code.R
@@ -1,23 +1,25 @@
 # K2_code.R
+# Multivariate Regression
 # Exercises
 
 # Load packages
 library(dplyr)
 library(readr)
 library(broom)
-library(texreg)
-library(gtools)
+library(texreg) # for texreg
+library(gtools) # for our tidier() function
 
 
 # Our data about disaster outcomes in Japanese municipalities over time
-cities = read_csv("workshops/jp_matching_experiment.csv") %>% 
+cities = read_csv("K/jp_matching_experiment.csv") %>% 
   # Tell R to treat year and pref as **ordered categories**
   mutate(year = factor(year),
          pref = factor(pref),
-         by_tsunami = factor(by_tsunami, levels = c("Not Hit", "Hit")))
+         by_tsunami = factor(by_tsunami, levels = c("Not Hit", "Hit")) )
 
 cities %>% glimpse()
 
+head(cities)
 
 
 # Let's write a little tidier function..
@@ -52,25 +54,47 @@ cities %>% lm(formula = income_per_capita ~ 1)
 cities %>% summarize(mean = mean(income_per_capita))
 
 
+# Answers:
+# An intercept-only model predicts the outcome 
+# by literally taking the mean of the outcome.
+
 
 # 2. Model the effect of each year on income. 
 # Which year is not represented? The intercept represents that baseline category.
 cities$year %>% unique()
-m = cities %>% lm(formula = income_per_capita ~ year)
+m = lm(formula = income_per_capita ~ year, data = cities)
 m 
 # Calculate the predicted income per capita from 2011 to 2017.
+
+
+# Answers:
+# 2011 is not represented; 2011 is the baseline category for comparison.
+# 1.0564901 + -0.0335762 --> 1.022914 in 2012
+# ...
+# 1.0564901 + 0.1021192  --> 1.158609 in 2017
 
 
 
 # 3. Estimate the effect of being hit by the tsunami on income per capita, 
 # controlling for damage rates.
-m = cities %>% lm(formula = income_per_capita ~  damage_rate + by_tsunami)
+m = lm(formula = income_per_capita ~  damage_rate + by_tsunami, data = cities)
 tidier(m)
 
 # Report the effect of being hit by the tsunami.
 # As [X] increases by 1 [unit], [Y] increases by [BETA] [units].
 # This effect has a 95% confidence interval from [A] to [B].
 # This effect is statistically [significant? insignificant?] with a p-value of [XXX].
+
+
+# Answers:
+
+# Report the effect of being hit by the tsunami.
+# When hit by the tsunami (1 vs. 0), the income per capita in millions of yen per capita 
+# increases by +0.032 millions of yen per capita.
+# This effect has a 95% confidence interval from -0.001 to 0.066.
+# This effect is statistically **insignificant?** at a 95% confidence level, with a p-value of 0.058.
+# This effect is statistically significant at a 90% confidence level, because 0.058 < p = 0.10.
+
 
 
 
@@ -85,6 +109,9 @@ m2 = cities %>% lm(formula = income_per_capita ~ pop_density + unemployment +
 # View the resulting statistical table. 
 # How does the information change when we control for year vs. each year? 
 texreg::screenreg(l = list(m1, m2))
+
+
+# See video for discussion.
 
 
 
@@ -104,6 +131,24 @@ cities %>%
 # Which effect sizes can you NOT compare?
 
 
+
+
+# Answers:
+
+# As the population density increases by 1 standard deviation,
+# the income per capita is expected to increase by 0.18 millions of yen per capita.
+
+# As the unemployment rate increases by 1 standard deviation,
+# the income per capita is expected to decrease by 0.05 millions of yen per capita.
+
+# As the damage rate increases by 1 standard deviation,
+# the income per capita is expected to decrease by 0.01 millions of yen per capita.
+
+# You can compare pop_density, unemployment, and damage_rate,
+# because they are all in units of standard deviations.
+
+# You can't compare effects of numeric predictors against effects of categorical predictors,
+# because they're fundamentally different - eg. standard deviations versus 0/1
 
 
 # Cleanup!
--- K/K2_code.py
+++ K/K2_code.py
@@ -1,5 +1,7 @@
 # K2_code.py
+# Multivariate Regression in Python
 # Exercise
+
 
 # Load packages
 import pandas as pd
@@ -8,6 +10,7 @@ import statsmodels.api as sm
 
 # Our data about disaster outcomes in Japanese municipalities over time
 cities = pd.read_csv("K/jp_matching_experiment.csv")
+
 # Make by_tsunami a binary variable where 1 means hit by tsunami
 cities['by_tsunami'] = cities.by_tsunami == "Hit"
 # Make categorical variable into a series of binary variables per year
@@ -22,6 +25,7 @@ cities['year_2017'] = cities.year == 2017
 
 # View columns
 cities.columns
+
 # View data
 cities
 
@@ -39,11 +43,35 @@ cities.income_per_capita.mean()
 
 
 
+
+# Answers:
+# An intercept-only model predicts the outcome 
+# by literally taking the mean of the outcome.
+
+
+
+
+
+
+
 # 2. Model the effect of each year on income. 
 # Which year is not represented? The intercept represents that baseline category.
 cities.year.unique() # see unique values
 m = sm.formula.ols(formula = 'income_per_capita ~ year_2012 + year_2013 + year_2014 + year_2015 + year_2016 + year_2017', data = cities).fit()
 m.summary()
+
+
+
+
+
+
+# Answers:
+# 2011 is not represented; 2011 is the baseline category for comparison.
+# 1.056 + -0.0336 --> 1.0224 in 2012
+# ...
+# 1.056 + 0.1021  --> 1.1581 in 2017
+
+
 
 
 
@@ -64,6 +92,24 @@ m.summary()
 
 
 
+
+
+
+# Answers:
+
+# Report the effect of being hit by the tsunami.
+# When hit by the tsunami (1 vs. 0), the income per capita in millions of yen per capita 
+# increases by +0.032 millions of yen per capita.
+# This effect has a 95% confidence interval from -0.001 to 0.066.
+# This effect is statistically **insignificant?** at a 95% confidence level, with a p-value of 0.058.
+# This effect is statistically significant at a 90% confidence level, because 0.058 < p = 0.10.
+
+
+
+
+
+
+
 # 4. Model the effect of time on income per capita, 
 # controlling for relevant traits.
 m1 = sm.formula.ols(formula = 'income_per_capita ~ pop_density + unemployment + damage_rate + by_tsunami + year', data = cities).fit()
@@ -77,6 +123,7 @@ m1.summary()
 m2.summary()
 
 
+# See video for discussion.
 
 
 
@@ -92,14 +139,41 @@ cities2 = cities
 cities2['pop_density'] = scale(cities.pop_density)
 cities2['unemployment'] = scale(cities.unemployment)
 cities2['damage_rate'] = scale(cities.damage_rate)
+
+# cities2.damage_rate.describe()
+
 m = sm.formula.ols(
     formula = 'income_per_capita ~ pop_density + unemployment + damage_rate + by_tsunami + year_2012 + year_2013 + year_2014 + year_2015 + year_2016 + year_2017', 
     data = cities2).fit()
+
+m.summary()
 
 # Report the population density vs. unemployment, damage_rate
 # As [X] increases by 1 [unit], [Y] increases by [BETA] [units].
 # Which effect size is largest?
 # Which effect sizes can you NOT compare?
+
+
+
+
+
+# Answers:
+
+# As the population density increases by 1 standard deviation,
+# the income per capita is expected to increase by 0.18 millions of yen per capita.
+
+# As the unemployment rate increases by 1 standard deviation,
+# the income per capita is expected to decrease by 0.05 millions of yen per capita.
+
+# As the damage rate increases by 1 standard deviation,
+# the income per capita is expected to decrease by 0.01 millions of yen per capita.
+
+# You can compare pop_density, unemployment, and damage_rate,
+# because they are all in units of standard deviations.
+
+# You can't compare effects of numeric predictors against effects of categorical predictors,
+# because they're fundamentally different - eg. standard deviations versus 0/1
+
 
 
 # Cleanup!
